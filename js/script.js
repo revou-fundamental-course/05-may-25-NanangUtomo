93897356d128c9ce59219f8eb3f30d590a71f7e9
Mochammad Nanang Utomo <mochammadnanangu19@gmail.com>
2025-05-15 01:12:16 +0700

    Tambah menu Tentang Kami dan Destinasi

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar sidebar untuk menghilangkan Nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
